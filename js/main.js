6219e5ed9bb741f7cc8a2e520159915df190d430
Martin Piazzon <mpiazzon@hotmail.com>
2021-02-25 05:23:05 -0300

    .

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function t() {
+  var typed = new Typed(".typed", {
+    stringsElement: ".typed-strings",
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1500,
+  });
+}
+
+t();
